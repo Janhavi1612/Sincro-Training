4cf317e54a56741deb5f27b99e00414696b1763b
Janhavi1612 <janhavimd1612@gmail.com>
2020-09-10 15:21:04 +0530

    initial commit -greeting kata first 6 requirements

---
+++ TestDrivenDevelopment/GreetingKata/src/Greeter.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+
+public class Greeter {
+
+	public String greet(String string) {
+		if (string.equals("")) {
+			string = "my friend";
+		}
+		if (isUpperCase(string)) {
+			return "HELLO " + string + "!";
+		}
+		return "Hello, " + string;
+	}
+
+	public static boolean isUpperCase(String string) {
+		for (int i = 0; i < string.length(); i++) {
+			if (!Character.isUpperCase(string.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public String greet(String[] strings) {
+		ArrayList<String> normalStrings = new ArrayList<String>();
+		ArrayList<String> shoutStrings = new ArrayList<String>();
+		for (int i = 0; i < strings.length; i++) {
+			if (!isUpperCase(strings[i])) {
+				normalStrings.add(strings[i]);
+			} else {
+				shoutStrings.add(strings[i]);
+			}
+		}
+		String hello = "Hello";
+		if (strings.length == 2) {
+			hello = hello + " ";
+		} else {
+			hello = hello + ", ";
+		}
+		return hello + greetNormal(normalStrings) + greetShouting(shoutStrings);
+
+		/*
+		 * String outputNormal = "Hello, "; String outputShouting = ""; for (int i = 0;
+		 * i < strings.length; i++) { if (strings.length == 2) { return "Hello " +
+		 * strings[0] + " and " + strings[1] + "."; } else { if
+		 * (!isUpperCase(strings[i])) { if (i + 1 == strings.length) { outputNormal =
+		 * outputNormal + "and " + strings[i]; } else { outputNormal = outputNormal +
+		 * strings[i] + ", "; } } else { outputShouting = outputShouting + " AND HELLO "
+		 * + strings[i] + "!"; } } } return outputNormal + "." + outputShouting;
+		 */
+	}
+
+	private String greetShouting(ArrayList<String> shoutStrings) {
+		String output = "";
+		for (int i = 0; i < shoutStrings.size(); i++) {
+			output = output + " AND HELLO " + shoutStrings.get(i) + "!";
+		}
+		return output;
+	}
+
+	private String greetNormal(ArrayList<String> normalStrings) {
+		if (normalStrings.size() == 2) {
+			return normalStrings.get(0) + " and " + normalStrings.get(1) + ".";
+		} else {
+			String output = "";
+			for (int i = 0; i < normalStrings.size(); i++) {
+				if (i + 1 == normalStrings.size()) {
+					output = output + "and " + normalStrings.get(i) + ".";
+				} else {
+					output = output + normalStrings.get(i) + ", ";
+				}
+			}
+			return output;
+		}
+	}
+
+}
---
+++ TestDrivenDevelopment/GreetingKata/src/GreeterTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GreeterTest {
+	Greeter greeter;
+
+	@BeforeEach
+	void setup() {
+		greeter = new Greeter();
+	}
+
+	@Test
+	void shouldReturnHelloName() {
+		Assertions.assertEquals("Hello, Bob", greeter.greet("Bob"));
+	}
+
+	@Test
+	void shouldReturnHellomyFriendForNullInput() {
+		Assertions.assertEquals("Hello, my friend", greeter.greet(""));
+	}
+
+	@Test
+	void shouldShoutBackForUppercaseName() {
+		Assertions.assertEquals("HELLO JERRY!", greeter.greet("JERRY"));
+	}
+
+	@Test
+	void shouldHandleTwoNameInput() {
+		Assertions.assertEquals("Hello Jill and Jane.", greeter.greet(new String[] { "Jill", "Jane" }));
+	}
+
+	@Test
+	void shouldHandleArbitraryNumberOfInputs() {
+		Assertions.assertEquals("Hello, Amy, Brian, and Charlotte.",
+				greeter.greet(new String[] { "Amy", "Brian", "Charlotte" }));
+	}
+
+	@Test
+	void shouldHandleMixOfNormalAndShouting() {
+		Assertions.assertEquals("Hello, Amy and Charlotte. AND HELLO BRIAN!",
+				greeter.greet(new String[] { "Amy", "BRIAN", "Charlotte" }));
+	}
+}
