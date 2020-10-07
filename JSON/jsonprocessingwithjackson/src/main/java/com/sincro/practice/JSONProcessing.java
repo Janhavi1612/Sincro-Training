d9bd7f6f442b90791b300b609e86899ce49aa576
janhavi1612 <janhavimd1612@gmail.com>
2020-10-07 16:48:34 +0530

    added testcases

--- JSON/jsonprocessingwithjackson/src/main/java/com/sincro/practice/JSONProcessing.java
+++ JSON/jsonprocessingwithjackson/src/main/java/com/sincro/practice/JSONProcessing.java
@@ -8,11 +8,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class JSONProcessing {
-    private static String filePath = "offers.json";
+    private static String filePath = "/Users/dabakj/Janhavi1612LocalRepo/JSON/jsonprocessingwithjackson/src/main/resources/offers.json";
     public static void main(String[] args) throws IOException {
         byte[] jsonData = Files.readAllBytes(Paths.get(filePath));
         ObjectMapper objectMapper = new ObjectMapper();
         OffersJsonRoot rootOffers = objectMapper.readValue(jsonData, OffersJsonRoot.class);
-
     }
 }
